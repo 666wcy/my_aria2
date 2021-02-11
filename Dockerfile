bab317af9ca9c90a2a8b53c55d80104d5d5048df
666wcy <769020367@qq.com>
2021-02-11 12:42:27 +0800

    new

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,18 @@
 FROM ubuntu
 
 
-COPY root /
+
+
 RUN apt-get update
 RUN apt-get install sudo
 RUN sudo apt-get update
 RUN apt-get install aria2 -y
 
+RUN mkdir /root/.aria2
+COPY config /root/.aria2/
 
+COPY root /
+RUN sudo chmod 777 /root/.aria2/
 RUN sudo chmod 777 /rclone
 RUN mv /rclone /usr/bin/
 
--- root/start.sh
+++ root/start.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-mkdir /config/
-mkdir /config/aria2/
-cp /aria2.conf /config/aria2/
+chmod 0777 /root/.aria2/ -R
+mkdir /.config/
+mkdir /.config/rclone
+touch /.config/rclone/rclone.conf
+echo "$Rclone" >>/.config/rclone/rclone.conf
 
-mkdir /config/.aria2/
-touch /config/.aria2/aria2.session
-
-aria2c --conf-path=/config/aria2/aria2.conf --rpc-listen-port=$PORT
+aria2c --conf-path=/config/aria2/aria2.conf --rpc-listen-port=$PORT --rpc-secret=$Secret
