3233c2049204121c1b00a19817f99e091a0a86ce
666wcy <769020367@qq.com>
2021-02-11 11:58:15 +0800

    Update start.sh

--- root/start.sh
+++ root/start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-sudo mkdir /config/
-sudo mkdir /config/aria2/
+mkdir /config/
+mkdir /config/aria2/
 cp /aria2.conf /config/aria2/
 
-sudo mkdir /config/.aria2/
+mkdir /config/.aria2/
 touch /config/.aria2/aria2.session
 
 aria2c --conf-path=/config/aria2/aria2.conf --rpc-listen-port=$PORT
